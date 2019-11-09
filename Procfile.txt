44700051f16e82f0faa4fed55200dbf5401097b0
Your Name <you@example.com>
2019-11-09 15:33:23 +0800

    my mynewdj app

---
+++ Procfile.txt
@@ -0,0 +1,1 @@
+﻿web: gunicorn --pythonpath mysite mysite.wsgi
---
+++ trips/views.py
@@ -0,0 +1,31 @@
+#from django.shortcuts import render
+
+# Create your views here.
+
+ # trips/views.py
+
+#from django.http import HttpResponse
+
+
+#def hello_world(request):
+#    return HttpResponse("Hello World!")
+
+
+ # trips/views.py
+
+from datetime import datetime
+from django.shortcuts import render
+from .models import Post
+
+def hello_world(request):
+    return render(request, 'hello_world.html', {
+        'current_time': str(datetime.now()),
+    })
+def home(request):
+    post_list = Post.objects.all()
+    return render(request, 'home.html', {
+        'post_list': post_list,
+    })
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'post.html', {'post': post})
